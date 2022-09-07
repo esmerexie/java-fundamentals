74a0248393cf8a3807f9df3df954359d965d5345
esmerexie <esmeraldarexie@gmail.com>
2022-09-06 23:18:04 -0700

    pushing up files

--- test/src/main.java
+++ test/src/main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class main {
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class main {
+    String textToAppend = "Happy Learning !!";
+
+    Path path = Paths.get("c:/temp/samplefile.txt");
+
+Files.write(path, textToAppend.getBytes(), StandardOpenOption.APPEND);
 }
