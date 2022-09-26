000a1524ad12622d030d6fa413eceaf25c3b6301
esmerexie <esmeraldarexie@gmail.com>
2022-09-26 08:53:10 -0700

    somewhat of a hashmap layout

---
+++ linked-list/lib/src/main/java/hashTable/HashMapPair.java
@@ -0,0 +1,10 @@
+package hashTable;
+
+import java.util.AbstractMap;
+
+public class HashMapPair<K, V> extends AbstractMap.SimpleEntry {
+
+    public HashMapPair(K key, V value){
+        super(key, value);
+    }
+}
